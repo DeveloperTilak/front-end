fb230b590bb1cc2df1c0ac24f8c1259e82861e1b
Developer Awdhesh <awdheshthinker@gmail.com>
2023-08-07 16:08:20 +0530

    basic UI done

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,9 +1,76 @@
-import React from 'react';
+import { Box, Center, Flex, Heading, Text } from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function Blogs(props) {
+
+    const [blog, setBlog] = useState([])
+    const [loading, setLoading]  = useState(true)
+
+    useEffect(()=>{
+
+
+        const token = localStorage.getItem("token")
+        console.log(token)
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        };
+
+        try {
+            
+            axios.get(`https://courageous-puce-bass.cyclic.app/blog`, config).then((res)=>{
+                console.log(res)
+                setBlog(res.data.blog)
+                setLoading(false)
+                // console.log(res.data.blog)
+            })
+        } catch (error) {
+            console.log("An Error Occurred while fetching data: ", error)
+
+            
+        }
+
+
+    },[])
+
+    if(loading){
+        return (
+            <Center>
+                <Text>Loading...</Text>
+            </Center>
+        )
+    }
     return (
         <div>
-            <h2>this is blog</h2>
+            <Center>
+                
+                <Heading>This is blog</Heading>
+
+
+            </Center>
+
+
+            <Box>
+                <Flex mt={"2rem"} justifyContent={"center"} align={"center"}>
+
+                    {
+                        blog.length>0 ? (
+                            blog.map((element, index)=>{
+                                return(
+                                    <Box key={index} w={"165px"} height={"100px"}>
+                                        <Heading fontSize={"15px"}>{element.title}</Heading>
+                                        <Text>{element.description}</Text>
+                                        </Box>
+                                )
+                            })
+                        ): <Text> there is no blog </Text>
+                    }
+                </Flex>
+
+            </Box>
         </div>
     );
 }
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import { FormControl, FormLabel, Input , Button } from "@chakra-ui/react";
+import axios from "axios";
+ 
+import React, { useState } from "react";
 
+const initialValues = {
+  email: "",
+  password: "",
+};
 function Login(props) {
-    return (
-        <div>
-            <h3>this is login</h3>
-        </div>
-    );
+  const [data, setData] = useState(initialValues);
+
+
+
+  const handleChange  = (e)=>{
+    setData({...data, [e.target.name]: e.target.value})
+
+  }
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    // console.log(data)
+
+   try {
+
+    const res = await  axios.post("https://courageous-puce-bass.cyclic.app/user/login", data)
+    console.log(res)
+
+    const token = res.data.token;
+    console.log(token)
+    localStorage.setItem("token", token)
+    setData(initialValues)
+    
+   } catch (error) {
+    console.log("An error occurred while submitting the form.")
+    console.error("Error: ", error);
+    
+   }
+
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+      <FormControl >
+        <FormLabel>Email</FormLabel>
+        <Input
+          type="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <FormLabel>Password</FormLabel>
+        <Input
+          type="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <Button type="submit">Submit</Button>
+      </FormControl>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
